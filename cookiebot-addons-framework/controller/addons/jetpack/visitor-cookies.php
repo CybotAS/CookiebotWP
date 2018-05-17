9ab844a69d1483bcc9671865638dff9d2a1f1f30
Aytac Kokus <aytac@appsaloon.be>
2018-05-17 15:26:07 +0200

    stop akm_mobile when preferences consent is not given

--- cookiebot-addons-framework/controller/addons/jetpack/visitor-cookies.php
+++ cookiebot-addons-framework/controller/addons/jetpack/visitor-cookies.php
@@ -9,14 +9,19 @@ class Visitor_Cookies {
 		 * When preferences consent is not given
 		 * Then disable comment cookies
 		 */
-		if( !cookiebot_is_cookie_state_accepted('preferences') ) {
+		if ( ! cookiebot_is_cookie_state_accepted( 'preferences' ) ) {
 			$this->disable_comment_cookies();
+			$this->do_not_save_mobile_or_web_view();
 		}
 	}
 
 	/**
 	 * Disable cookie comments
 	 *
+	 * Cookies:
+	 * - comment_author_{HASH}
+	 * - comment_author_email_{HASH}
+	 * - comment_author_url_{HASH}
 	 * @since 1.2.0
 	 */
 	protected function disable_comment_cookies() {
@@ -29,5 +34,33 @@ class Visitor_Cookies {
 		 * Remove action comment cookies in wordpress core
 		 */
 		remove_action( 'set_comment_cookies', 'wp_set_comment_cookies' );
+	}
+
+	/**
+	 * Doesn't save the visitor wish in cookie
+	 *
+	 * Cookie:
+	 * - akm_mobile
+	 *
+	 * @since 1.2.0
+	 */
+	protected function do_not_save_mobile_or_web_view() {
+		remove_action( 'init', 'jetpack_mobile_request_handler' );
+
+		/**
+		 * Show message to accept preferences consent to save
+		 */
+		add_action( 'wp_mobile_theme_footer', array( $this, 'view_accept_preferences_consent' ) );
+	}
+
+	/**
+	 * Display message to enable
+	 *
+	 * @since 1.2.0
+	 */
+	public function view_accept_preferences_consent() {
+		echo '<div class="cookieconsent-optout-preferences">
+						  ' . __( 'Please <a href="javascript:Cookiebot.renew()">accept preferences-cookies</a> to switch to full page.', 'cookiebot_addons' ) . '
+						</div>';
 	}
 }
