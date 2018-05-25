5de3fa78e371b20ce87eb1dd4a9f50a53018e737
Aytac Kokus <aytac@appsaloon.be>
2018-05-25 15:19:12 +0200

    undo removing not used classes

--- controller/plugin-controller.php
+++ controller/plugin-controller.php
@@ -52,10 +52,6 @@ class Plugin_Controller {
 			 */
 			if ( $addon->is_addon_enabled() && $addon->is_plugin_installed() ) {
 				$addon->load_configuration();
-			} else {
-				// unset not used addon
-				$this->container->set( $plugin->class, '' );
-				unset( $addon );
 			}
 		}
 
