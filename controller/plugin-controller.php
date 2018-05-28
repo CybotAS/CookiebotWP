f502c885bf975d415063b8d2659f7931c3164fc7
Johan Holst Nielsen <johan@phpgeek.dk>
2018-05-28 08:28:53 +0200

    Initial commit feature UI - without custom settings

--- controller/plugin-controller.php
+++ controller/plugin-controller.php
@@ -20,6 +20,7 @@ class Plugin_Controller {
 	 */
 	public function check_addons() {
 		$this->load_plugins();
+		$activeAddons = $this->get_active_addons();
 
 		if ( ! function_exists( 'is_plugin_active' ) ){
 			require_once( ABSPATH . '/wp-admin/includes/plugin.php' );
@@ -27,14 +28,174 @@ class Plugin_Controller {
 
 		foreach ( $this->plugins as $plugin_class => $plugin ) {
 			/**
-			 * Load addon code if the plugin is active
+			 * Load addon code if the plugin is active and addon is activated
 			 */
 			if ( is_plugin_active( $plugin->file ) ) {
 				$this->load_addon( $plugin->class );
 			}
 		}
 	}
-
+	
+	/**
+	 * Load list of addons with status
+	 *
+	 * @since 1.2.0
+	 */
+	 
+	public function load_active_addons() {
+		$addons = $this->get_addon_list();
+		foreach( $addons['available'] as $plugin_class=>$plugin ) {
+			if ( is_plugin_active( $plugin['file'] ) ) {
+				$this->load_addon( $plugin['class'] );
+			}
+		}
+	}
+	
+	/**
+	 * Load list of addons with status
+	 *
+	 * @since 1.2.0
+	 */
+	public function get_addon_list() {
+		$addons = [];
+		
+		$this->load_plugins();
+		if ( ! function_exists( 'is_plugin_active' ) ) {
+			require_once( ABSPATH . '/wp-admin/includes/plugin.php' );
+		}
+		foreach ( $this->plugins as $plugin_class => $plugin ) {
+			$addons[(is_plugin_active( $plugin->file ) ? 'available' : 'unavailable')][$plugin_class] = [
+					'name'=>$plugin->name,
+					'file'=>$plugin->file,
+					'class'=>$plugin->class,
+					'available'=> ( is_plugin_active( $plugin->file ) ? true : false ),
+				];
+		}
+		return $addons;
+	}
+	
+	
+	/**
+	 * Get list of active addons
+	 *
+	 * @since 1.2.0
+	 */
+	public function get_active_addons() {
+		$activePlugins = get_option('cookiebot-addons-activated','');
+		if(!empty($activePlugins)) {
+			return explode(';',$activePlugins);
+		}
+		return false;
+	}
+	
+	/** 
+	 * Register settings for addons
+	 * 
+	 * @since 1.2.0
+	 */
+	/*public function register_setting() {
+		$this->load_plugins();
+		if ( ! function_exists( 'is_plugin_active' ) ) {
+			require_once( ABSPATH . '/wp-admin/includes/plugin.php' );
+		}
+		
+		foreach ( $this->plugins as $plugin_class => $plugin ) {
+			
+		}
+	}*/
+	
+	/**
+	 * Add menu for addon settings
+	 * 
+	 * @since 1.2.0
+	 */
+	public function add_menu() {
+		add_options_page( 'Cookiebot Addons', __('Cookiebot Addons','cookiebot-addons'), 'manage_options', 'cookiebot-addons', array($this,'setting_page'));
+	}
+	
+	/**
+	 * Settign page for Cookiebot addons
+	 *
+	 * @since 1.2.0
+	 */
+	function setting_page() {
+		if(isset($_GET['action']) && ($_GET['action'] == 'deactivate' || $_GET['action'] == 'activate')) {
+			$active = ( $_GET['action'] == 'activate' ) ? 'yes' : 'no';
+			update_option('cookiebot-addons-active-'.sanitize_key($_GET['addon']),$active);
+			
+			
+			$status = ($active == 'yes') ? 'The addon is now activated' : 'The addon is now deactivated';
+			?>
+			<div class="updated notice is-dismissible">
+        <p><?php _e( $status, 'cookiebot-addons' ); ?></p>
+			</div>
+			<?php
+		}
+		$addons = $this->get_addon_list();
+		
+		?>
+		<div class="wrap">
+			<h1><?php _e('Cookiebot Addons','cookiebot-addons'); ?></h1>
+			<p>
+				<?php _e('Below is a list of addons for Cookiebot. Addons help you making contributed plugins GDPR compliant.','cookiebot-addons'); ?>
+				<br />
+				<?php _e('Deactive addons if you want to handle GDPR compliance yourself or using another plugin.','cookiebot-addons'); ?>
+			</p>
+			<?php
+			
+			foreach($addons['available'] as $plugin_class=>$plugin) {
+				?>
+				<div class="postbox cookiebot-addon">
+					<h2><?php echo $plugin['name']; ?></h2>
+					<?php
+					if(get_option('cookiebot-addons-active-'.sanitize_key($plugin_class),'yes') == 'yes') {
+						?>
+						<a href="<?php echo admin_url('options-general.php?page=cookiebot-addons&action=deactivate&addon='.$plugin_class); ?>">
+							<?php _e('Deactivate addon','cookiebot-addons'); ?>
+						</a>
+						<?php
+					}
+					else {
+						?>
+						<a href="<?php echo admin_url('options-general.php?page=cookiebot-addons&action=activate&addon='.$plugin_class); ?>">
+							<?php _e('Activate addon','cookiebot-addons'); ?>
+						</a>
+						<?php
+					}
+					?>
+				</div>
+				<?php
+			}
+			?>
+			<h2><?php _e('Unavailable Addons','cookiebot-addons'); ?></h2>
+			<p>
+				<?php _e('Following addons are unavailable. This is usual because the addon is not useable because the main plugin is not activated.'); ?>
+			</p>
+			<?php
+			foreach($addons['unavailable'] as $plugin_class=>$plugin) {
+				?>
+				<div class="postbox cookiebot-addon">
+					<h2><?php echo $plugin['name']; ?></h2>
+					<i><?php _e('Unavailable','cookiebot-addons'); ?></i>
+				</div>
+				<?php
+			}
+			?>
+		</div>
+		<?php
+	}
+	
+	/**
+	 * 
+	 * 
+	 */
+	 
+	public function add_wp_admin_style($hook) {
+		if( $hook != 'settings_page_cookiebot-addons' ) { return; }
+		//die(__FILE__);
+		wp_enqueue_style( 'cookiebot_addons_custom_css', plugins_url('admin_styles.css',dirname(__FILE__)) );
+	}
+	
 	/**
 	 * Loads plugins from json file
 	 *
@@ -45,8 +206,10 @@ class Plugin_Controller {
 	 * @since 1.1.0
 	 */
 	private function load_plugins() {
-		$file          = file_get_contents( CAF_DIR . 'addons.json' );
-		$this->plugins = json_decode( $file );
+		if( empty( $this->plugins ) ) {
+			$file          = file_get_contents( CAF_DIR . 'addons.json' );
+			$this->plugins = json_decode( $file );
+		}
 	}
 
 	/**
--- cookiebot-addons-framework.php
+++ cookiebot-addons-framework.php
@@ -3,7 +3,7 @@
  * Plugin Name: Cookiebot Addons Framework
  * Description: Adding support for Cookiebot
  * Author: Johan Holst Nielsen & Aytac Kokus
- * Version: 1.1.0
+ * Version: 1.2.0
  */
 
 namespace cookiebot_addons_framework;
@@ -39,8 +39,16 @@ class Cookiebot_Addons_Framework {
 		 *
 		 * @since 1.1.0
 		 */
-		add_action( 'plugins_loaded', array( new Plugin_Controller(), 'check_addons' ) );
+		$pc = new Plugin_Controller();
+		add_action( 'plugins_loaded', array($pc, 'load_active_addons' ) );
+		
+		if(is_admin()) {
+			//add_action('admin_init', array($pc,'register_settings'));
+			add_action( 'admin_menu', array($pc,'add_menu'));
+			add_action( 'admin_enqueue_scripts', array($pc,'add_wp_admin_style') );
+		}
 	}
+	
 }
 
 /**
