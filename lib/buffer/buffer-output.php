87e4f99a7588ae4b0284ad8b0cec166e3f9d41a7
Johan Holst Nielsen <johan@phpgeek.dk>
2018-05-30 13:35:52 +0200

    force no cache in output buffering

--- lib/buffer/buffer-output.php
+++ lib/buffer/buffer-output.php
@@ -20,7 +20,7 @@ class Buffer_Output implements Buffer_Output_Interface {
 	 *
 	 * @since 1.2.0
 	 */
-	public function add_tag( $tag_name, $priority, $keywords = array(), $use_cache=true) {
+	public function add_tag( $tag_name, $priority, $keywords = array(), $use_cache = true) {
 		$tag       = new Buffer_Output_Tag( $tag_name, $priority, $keywords, $use_cache );
 		$unique_id = $tag->tag . '_' . $tag->priority;
 
