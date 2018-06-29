1013ac96b4c69eff26c11247f82a728911833953
Aytac Kokus <aytac@appsaloon.be>
2018-06-29 10:27:41 +0200

    remove wordpress test framework

--- tests/unit/test-get-option-name.php
+++ tests/unit/test-get-option-name.php
@@ -2,11 +2,6 @@
 
 namespace cookiebot_addons_framework\tests\unit;
 
-use cookiebot_addons_framework\lib\Settings_Service;
-use DI;
-use DI\Container;
-use DI\ContainerBuilder;
-
 class Test_Get_Option_Name extends \WP_UnitTestCase {
 
 	protected $plugins;
@@ -17,8 +12,6 @@ class Test_Get_Option_Name extends \WP_UnitTestCase {
 		Parent::setUp();
 
 		$this->get_plugins();
-
-		$this->build_container();
 	}
 
 	/**
@@ -27,40 +20,6 @@ class Test_Get_Option_Name extends \WP_UnitTestCase {
 	private function get_plugins() {
 		$file          = file_get_contents( CAF_DIR . 'addons.json' );
 		$this->plugins = json_decode( $file );
-	}
-
-	/**
-	 * Build IoC container
-	 *
-	 * @since 1.3.0
-	 */
-	protected function build_container() {
-		$builder = new ContainerBuilder();
-
-		$builder->addDefinitions( [
-			'Script_Loader_Tag_Interface' => DI\object( 'cookiebot_addons_framework\lib\script_loader_tag\Script_Loader_Tag' ),
-			'Cookie_Consent_Interface'    => DI\object( 'cookiebot_addons_framework\lib\Cookie_Consent' ),
-			'Buffer_Output_Interface'     => DI\object( 'cookiebot_addons_framework\lib\buffer\Buffer_Output' ),
-			'plugins'                     => DI\value( $this->plugins )
-		] );
-
-		$this->container = $builder->build();
-
-		$this->container->set( 'Settings_Service_Interface', DI\object( 'cookiebot_addons_framework\lib\Settings_Service' )
-			->constructor( $this->container ) );
-
-		foreach ( $this->plugins as $plugin ) {
-			/**
-			 * Load addon class to the container
-			 */
-			$this->container->set( $plugin->class, \DI\object( $plugin->class )
-				->constructor(
-					$this->container->get( 'Settings_Service_Interface' ),
-					$this->container->get( 'Script_Loader_Tag_Interface' ),
-					$this->container->get( 'Cookie_Consent_Interface' ),
-					$this->container->get( 'Buffer_Output_Interface' ) )
-			);
-		}
 	}
 
 	/**
@@ -80,7 +39,7 @@ class Test_Get_Option_Name extends \WP_UnitTestCase {
 		$options = array();
 
 		foreach ( $this->plugins as $plugin ) {
-			$p = $this->container->get( $plugin->class );
+			$p = new $plugin->class(null, null, null, null);
 
 			$this->assertNotContains( $p->get_option_name(), $options );
 
