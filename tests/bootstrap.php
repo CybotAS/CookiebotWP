9d2e41b9ea6d4233d1553170311de3e49a42a622
Aytac Kokus <aytac@appsaloon.be>
2018-06-29 14:31:45 +0200

    Fix phpunit bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -28,7 +28,7 @@ require_once $_tests_dir . '/includes/functions.php';
  */
 function _manually_load_plugin() {
 	// plugin
-	require dirname( dirname( __FILE__ ) ) . '/cookiebot-addons.php';
+	require dirname( dirname( __FILE__ ) ) . '/cookiebot-addons-init.php';
 }
 
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
