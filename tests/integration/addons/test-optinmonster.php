b4c59b454e52f6ab35e0d231aa4e33a4427ee68c
Marc Zijderveld <marc@level-level.com>
2018-10-16 11:55:50 +0200

    fix: Forgot to rename function name

--- tests/integration/addons/test-optinmonster.php
+++ tests/integration/addons/test-optinmonster.php
@@ -2,7 +2,7 @@
 
 namespace cookiebot_addons\tests\integration\addons;
 
-class Test_Jetpack_Twitter_Timeline_Widget extends \WP_UnitTestCase {
+class Test_Optinmonster extends \WP_UnitTestCase {
 	
 	public function setUp() {
 	
@@ -13,7 +13,7 @@ class Test_Jetpack_Twitter_Timeline_Widget extends \WP_UnitTestCase {
 	 *
 	 * @since 2.1.0
 	 */
-	public function test_twitter_timeline_widget() {
+	public function test_optinmonster() {
 		$content = file_get_contents('https://plugins.svn.wordpress.org/optinmonster/trunk/optin-monster-wp-api.php');
 		
 		$this->assertNotFalse( strpos( $content, 'wp_enqueue_script( \'optinmonster-api-script\' );' ) );
