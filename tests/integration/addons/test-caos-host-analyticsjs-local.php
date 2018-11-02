2c9c4961f3bf7c90118dcf2aa108b9ab7e827116
Johan Holst Nielsen <johan@phpgeek.dk>
2018-11-02 07:15:13 +0100

    Fix CAOS test

--- tests/integration/addons/test-caos-host-analyticsjs-local.php
+++ tests/integration/addons/test-caos-host-analyticsjs-local.php
@@ -3,7 +3,7 @@
 namespace cookiebot_addons\tests\integration\addons;
 
 class Test_Caos_Host_Analyticsjs_Local extends \WP_UnitTestCase {
-	
+
 	/**
 	 * This will validate if the hook "caos_analytics_render_tracking_code" still exists
 	 *
@@ -12,8 +12,8 @@ class Test_Caos_Host_Analyticsjs_Local extends \WP_UnitTestCase {
 	 * @since 2.1.0
 	 */
 	public function test_host_analyticsjs_local() {
-		$content = file_get_contents( 'http://plugins.svn.wordpress.org/host-analyticsjs-local/trunk/save-ga-local.php' );
-		
+		$content = file_get_contents( 'http://plugins.svn.wordpress.org/host-analyticsjs-local/trunk/host-analyticsjs-local.php' );
+
 		$this->assertNotFalse( strpos( $content,
 			'add_action(\'wp_footer\', \'caos_analytics_render_tracking_code\', $sgal_enqueue_order);' ) );
 		$this->assertNotFalse( strpos( $content,
