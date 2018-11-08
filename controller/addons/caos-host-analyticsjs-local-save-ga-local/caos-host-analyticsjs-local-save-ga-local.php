ca52883ccc46283b1fcc7a0df15fef0f7d25892d
Aytac Kokus <aytac@appsaloon.be>
2018-11-08 11:22:49 +0100

    Add more information about the new addon version

--- controller/addons/caos-host-analyticsjs-local-save-ga-local/caos-host-analyticsjs-local-save-ga-local.php
+++ controller/addons/caos-host-analyticsjs-local-save-ga-local/caos-host-analyticsjs-local-save-ga-local.php
@@ -4,6 +4,13 @@ namespace cookiebot_addons\controller\addons\caos_host_analyticsjs_local_save_ga
 
 use cookiebot_addons\controller\addons\caos_host_analyticsjs_local\CAOS_Host_Analyticsjs_Local;
 
+/**
+ * This is the older version of the plugin: CAOS_Host_Analyticsjs_Local
+ * Supports till the version: 2.0.0
+ *
+ * Class CAOS_Host_Analyticsjs_Local_Save_Ga_Local
+ * @package cookiebot_addons\controller\addons\caos_host_analyticsjs_local_save_ga_local
+ */
 class CAOS_Host_Analyticsjs_Local_Save_Ga_Local extends CAOS_Host_Analyticsjs_Local {
 
 	/**
