164d0cc3dbbbd98d646c09bd104d0f7b917ec341
Marc Zijderveld <marc@level-level.com>
2018-11-13 14:43:55 +0100

    fix: Url contained double http

--- tests/integration/addons/test-optinmonster.php
+++ tests/integration/addons/test-optinmonster.php
@@ -12,7 +12,7 @@ class Test_Optinmonster extends \WP_UnitTestCase {
 	 * @since 2.1.0
 	 */
 	public function test_optinmonster() {
-		$content = file_get_contents( 'http://http://plugins.svn.wordpress.org/optinmonster/trunk/optin-monster-wp-api.php' );
+		$content = file_get_contents( 'http://plugins.svn.wordpress.org/optinmonster/trunk/optin-monster-wp-api.php' );
 
 		$this->assertNotFalse( strpos( $content, "add_action( 'wp_enqueue_scripts', 'api-scripts' );") );
 	}	
