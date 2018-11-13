5c96fc1a7b6c4f360bee4fa892921731e5f4c121
Marc Zijderveld <marc@level-level.com>
2018-11-13 14:54:44 +0100

    fix: Fix quotes

--- tests/integration/addons/test-optinmonster.php
+++ tests/integration/addons/test-optinmonster.php
@@ -4,6 +4,10 @@ namespace cookiebot_addons\tests\integration\addons;
 
 class Test_Optinmonster extends \WP_UnitTestCase {
 
+	public function setUp() {
+	
+	}
+
 	/**
 	 * This will validate if the hook "api-scripts" still exists
 	 *
@@ -12,6 +16,6 @@ class Test_Optinmonster extends \WP_UnitTestCase {
 	public function test_optinmonster() {
 		$content = file_get_contents( 'http://plugins.svn.wordpress.org/optinmonster/trunk/optin-monster-wp-api.php' );
 
-		$this->assertNotFalse( strpos( $content, "add_action( 'wp_enqueue_scripts', 'api-scripts' );") );
+		$this->assertNotFalse( strpos( $content, 'add_action( \'wp_enqueue_scripts\', array( $this, \'api_script\' ) );') );
 	}	
 }
