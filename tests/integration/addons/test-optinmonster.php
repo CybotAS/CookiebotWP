7e159bd4346c1a4ec44a1982dfd44a6d2712cdf5
Marc Zijderveld <marc@level-level.com>
2018-11-13 14:33:52 +0100

    fix: Change hook for intergration test

--- tests/integration/addons/test-optinmonster.php
+++ tests/integration/addons/test-optinmonster.php
@@ -3,19 +3,16 @@
 namespace cookiebot_addons\tests\integration\addons;
 
 class Test_Optinmonster extends \WP_UnitTestCase {
-	
-	public function setUp() {
-	
-	}
-	
+
 	/**
-	 * This will validate if the hook "optinmonster" still exists
+	 * This will validate if the hook "caos_analytics_render_tracking_code" still exists
+	 *
+	 * @covers \cookiebot_addons\controller\addons\optinmonster\
 	 *
 	 * @since 2.1.0
 	 */
 	public function test_optinmonster() {
-		$content = file_get_contents('https://plugins.svn.wordpress.org/optinmonster/trunk/optin-monster-wp-api.php');
-		
-		$this->assertNotFalse( strpos( $content, 'add_action( \'wp_enqueue_scripts\', \'optinmonster-api-script\' );') );
-	}
+		$content = file_get_contents( 'http://http://plugins.svn.wordpress.org/optinmonster/trunk/optin-monster-wp-api.php' );
+
+		$this->assertNotFalse( strpos( $content, "add_action( 'wp_enqueue_scripts', 'api-scripts' );") );
 }
