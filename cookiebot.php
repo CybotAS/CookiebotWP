92535bee7e88969c4e16e3cfe787d762bd613a09
Johan Holst Nielsen <johan@phpgeek.dk>
2019-05-15 10:11:42 +0200

    Changing version to 2.4.2 and update readme

--- cookiebot.php
+++ cookiebot.php
@@ -4,7 +4,7 @@ Plugin Name: Cookiebot | GDPR Compliant Cookie Consent and Notice
 Plugin URI: https://cookiebot.com/
 Description: Cookiebot is a fully GDPR & ePrivacy compliant cookie consent solution supporting prior consent, cookie declaration, and documentation of consents. Easy to install, implement and configure.
 Author: Cybot A/S
-Version: 2.4.1
+Version: 2.4.2
 Author URI: http://cookiebot.com
 Text Domain: cookiebot
 Domain Path: /langs
