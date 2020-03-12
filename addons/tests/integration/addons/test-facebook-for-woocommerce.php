41aac82d42b71439673fc539a66696ff6335533b
Johan Holst Nielsen <johan@phpgeek.dk>
2020-03-12 12:16:53 +0100

    Update fb for woo test

--- addons/tests/integration/addons/test-facebook-for-woocommerce.php
+++ addons/tests/integration/addons/test-facebook-for-woocommerce.php
@@ -26,18 +26,14 @@ class Test_Facebook_For_Woocommerce extends \WP_UnitTestCase {
 		
 		$this->assertNotFalse( strpos( $content, 'add_action( \'wp_head\', array( $this, \'apply_filters\' ) );' ) );
 		$this->assertNotFalse( strpos( $content, 'add_action( \'woocommerce_after_single_product\', [ $this, \'inject_view_content_event\' ] );' ) );
-		$this->assertNotFalse( strpos( $content, 'add_action(
-				\'woocommerce_after_shop_loop\',' ) );
-		$this->assertNotFalse( strpos( $content, 'add_action(
-				\'pre_get_posts\',' ) );
-		$this->assertNotFalse( strpos( $content, 'add_action(
-				\'woocommerce_add_to_cart\',' ) );
+		$this->assertNotFalse( strpos( $content, 'array( $this, \'inject_view_category_event\' )' ) );
+		$this->assertNotFalse( strpos( $content, 'array( $this, \'inject_search_event\' )' ) );
+		$this->assertNotFalse( strpos( $content, 'add_action( \'woocommerce_add_to_cart\',             [ $this, \'inject_add_to_cart_event\' ], 40, 4 );' ) );
 		$this->assertNotFalse( strpos( $content, 'add_action( \'woocommerce_ajax_added_to_cart\', [ $this, \'add_filter_for_add_to_cart_fragments\' ] );' ) );
-		$this->assertNotFalse( strpos( $content, 'add_action(
-				\'woocommerce_after_checkout_form\',' ) );
-		$this->assertNotFalse( strpos( $content, 'add_action(
-				\'woocommerce_thankyou\',' ) );
-		$this->assertNotFalse( strpos( $content, 'add_action(
-				\'woocommerce_payment_complete\',' ) );
+		$this->assertNotFalse( strpos( $content, 'array( $this, \'inject_initiate_checkout_event\' )' ) );
+		$this->assertNotFalse( strpos( $content, 'array( $this, \'inject_gateway_purchase_event\' ),' ) );
+		$this->assertNotFalse( strpos( $content, 'array( $this, \'inject_purchase_event\' ),' ) );
+		$this->assertNotFalse( strpos( $content, 'const FB_PRIORITY_LOW     = 11;' ) );
+		$this->assertNotFalse( strpos( $content, 'const FB_PRIORITY_HIGH    = 2;' ) );
 	}
 }
