e4f71c065648f55b58fb565a72e6dbc3cfa17354
Martin Schadegg Brønniche <ms@morningtrain.dk>
2020-05-27 16:46:46 +0200

    Updated test description

--- addons/tests/integration/addons/test-simple-share-buttons-adder.php
+++ addons/tests/integration/addons/test-simple-share-buttons-adder.php
@@ -5,11 +5,11 @@
 	class Test_Simple_Share_Buttons_Adder extends \WP_UnitTestCase {
 
 		/**
-		 * This will cover the existince of the wp_enqueue_script addtoany
+		 * This will cover the existince of the wp_enqueue_script ssba-sharethis
 		 *
-		 * @covers \cookiebot_addons\controller\addons\add_to_any\Add_To_Any
+		 * @covers \cookiebot_addons\controller\addons\simple_share_buttons_adder\Simple_Share_Buttons_Adder
 		 *
-		 * @since 2.1.0
+		 * @since 3.6.5
 		 */
 		public function test_script_loader_tag_addtoany() {
 			$content = file_get_contents( 'http://plugins.svn.wordpress.org/simple-share-buttons-adder/trunk/php/class-styles.php' );
