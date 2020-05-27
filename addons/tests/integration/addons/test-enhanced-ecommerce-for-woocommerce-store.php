dbffac3b5f3629feda4bca69d2718d17a2b7c491
Martin Schadegg Brønniche <ms@morningtrain.dk>
2020-05-27 16:44:56 +0200

    Added test for enhanced ecommerce addon

---
+++ addons/tests/integration/addons/test-enhanced-ecommerce-for-woocommerce-store.php
@@ -0,0 +1,19 @@
+<?php
+
+	namespace cookiebot_addons\tests\integration\addons;
+
+	class Test_Enhanced_Ecommerce_For_WooCommerce_Store extends \WP_UnitTestCase {
+
+		/**
+		 * This will cover the existince of the wc_enqueue_js
+		 *
+		 * @covers \cookiebot_addons\controller\addons\enhanced_ecommerce_for_woocommerce_store\Enhanced_Ecommerce_For_WooCommerce_Store
+		 *
+		 * @since 3.6.6
+		 */
+		public function test_script_loader_tag_addtoany() {
+			$content = file_get_contents( 'https://plugins.svn.wordpress.org/enhanced-e-commerce-for-woocommerce-store/trunk/public/class-enhanced-ecommerce-google-analytics-public.php' );
+
+			$this->assertNotFalse( strpos( $content, "wc_enqueue_js(" ) );
+		}
+	}
