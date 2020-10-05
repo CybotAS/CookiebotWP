846df14898fbd2bd1ef6c4ff898022cdb38bd594
lordlukas10 <lukas.tipsmark@gmail.com>
2020-10-05 11:13:13 +0200

    added ' around $data_layer

--- cookiebot.php
+++ cookiebot.php
@@ -1465,7 +1465,7 @@ final class Cookiebot_WP {
 			  var f = d.getElementsByTagName(s)[0],  j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : ''; 
 			  j.async = true; j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl; 
 			  f.parentNode.insertBefore(j, f);})
-			  (window, document, 'script', " . $data_layer . ", " . get_option('cookiebot-gtm-id') . ");
+			  (window, document, 'script', '" . $data_layer . "', '" . get_option('cookiebot-gtm-id') . "');
 			  </script>";
 
 			if($printTag===false) {
