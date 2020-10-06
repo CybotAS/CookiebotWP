12127fb39ed799517dbf755af994fcfb91d224f6
lordlukas10 <lukas.tipsmark@gmail.com>
2020-10-06 12:18:08 +0200

    added parentheses around links

--- cookiebot.php
+++ cookiebot.php
@@ -1185,7 +1185,7 @@ final class Cookiebot_WP {
 				<p><?php _e('Enable GTM', 'cookiebot')?></p>
 				<div class="GTM_check">
 					<input type="checkbox" name="cookiebot-gtm" id="cookiebot-gtm" value="1" <?php checked(1,get_option('cookiebot-gtm'), true); ?> style="float: left; margin: 2px 4px 0 0">
-					<a href="https://www.cookiebot.com/en/google-tag-manager-and-gdpr-compliance-with-cookiebot/" style="margin: 0; font-style: italic;"><?php _e('Learn more about GTM and cookiebot', 'cookiebot')?></a>
+					<a href="https://www.cookiebot.com/en/google-tag-manager-and-gdpr-compliance-with-cookiebot/" style="margin: 0; font-style: italic;">(<?php _e('Learn more about GTM and cookiebot', 'cookiebot')?>)</a>
 				</div>
 
 				<p><?php _e('GTM ID', 'cookiebot')?></p>
@@ -1200,7 +1200,7 @@ final class Cookiebot_WP {
 				<p><?php _e('Google Consent Mode', 'cookiebot')?></p>
 				<div class="GTM_check">
 					<input type="checkbox" name="cookiebot-gcm" id="gcm" value="1" <?php checked(1,get_option('cookiebot-gcm'), true); ?> style="float: left; margin: 2px 4px 0 0">
-					<a href="https://support.cookiebot.com/hc/en-us/articles/360016047000-Cookiebot-and-Google-Consent-Mode" style="margin: 0; font-style: italic;"><?php _e('Learn more about Google Consent Mode', 'cookiebot')?></a>
+					<a href="https://support.cookiebot.com/hc/en-us/articles/360016047000-Cookiebot-and-Google-Consent-Mode" style="margin: 0; font-style: italic;">(<?php _e('Learn more about Google Consent Mode', 'cookiebot')?>)</a>
 				</div>
 				<input type="submit" value="Save" name="gtm_save" style="background-color: rgb(0, 124, 186); color: white; padding: 5px 10px; border: none; border-radius: 5px; justify-self: start;">
 			</form>
