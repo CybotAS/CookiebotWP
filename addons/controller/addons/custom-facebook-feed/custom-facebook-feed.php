87eb189347ee879a73a717b57653cd6317fb4650
david <david@appsaloon.be>
2021-01-08 14:21:02 +0100

    fix(custom-facebook-feed): fixed buffer output and script loader tag for newer versions of the addon

--- addons/controller/addons/custom-facebook-feed/custom-facebook-feed.php
+++ addons/controller/addons/custom-facebook-feed/custom-facebook-feed.php
@@ -48,4 +48,31 @@ class Custom_Facebook_Feed extends Custom_Facebook_Feed_2_17_1 {
 		return 'http://plugins.svn.wordpress.org/custom-facebook-feed/trunk/custom-facebook-feed.php';
 	}
 
+    /**
+     * Manipulate the scripts if they are loaded.
+     *
+     * @since 1.1.0
+     */
+    public function cookiebot_addon_custom_facebook_feed() {
+
+        if(class_exists("\CustomFacebookFeed\Custom_Facebook_Feed")) {
+            $instance = \CustomFacebookFeed\Custom_Facebook_Feed::instance();
+
+            if ( has_action( 'wp_footer', array($instance, 'cff_js') ) ) {
+                /**
+                 * Consent not given - no cache
+                 */
+                $this->buffer_output->add_tag( 'wp_footer', 10, array( 'cfflinkhashtags' => $this->get_cookie_types() ), false );
+            }
+
+            // External js, so manipulate attributes
+            if ( has_action( 'wp_enqueue_scripts', array($instance, 'enqueue_scripts_assets') ) ) {
+                /**
+                 * Consent not given - no cache
+                 */
+                $this->script_loader_tag->add_tag( 'cffscripts', $this->get_cookie_types(), false );
+            }
+        }
+    }
+
 }
