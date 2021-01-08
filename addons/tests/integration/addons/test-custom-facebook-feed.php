ff6f82aa5052af57a65e42338dbdba40edf69b96
david <david@appsaloon.be>
2021-01-08 14:25:13 +0100

    fixed tests for customfacebook-feed and buffer-priorities

--- addons/tests/integration/addons/test-custom-facebook-feed.php
+++ addons/tests/integration/addons/test-custom-facebook-feed.php
@@ -14,19 +14,10 @@ class Test_Custom_Facebook_Feed extends Addons_Base {
 	 * @since 2.1.0
 	 */
 	public function test_host_analyticsjs_local() {
-		$inc_Custom_Facebook_Feed_content = $this->curl_get_content( 'http://plugins.svn.wordpress.org/custom-facebook-feed/trunk/custom-facebook-feed.php' );
+		$content = $this->curl_get_content( 'http://plugins.svn.wordpress.org/custom-facebook-feed/trunk/inc/Custom_Facebook_Feed.php' );
 
-		$this->assertNotFalse( strpos( $inc_Custom_Facebook_Feed_content, 'echo \'var cfflinkhashtags = "\' . $cff_link_hashtags . \'";\';') );
-		$this->assertNotFalse( strpos( $content, <<<EOD
-	    wp_register_script(
-	    	'cffscripts',
-	    	CFF_PLUGIN_URL . 'assets/js/cff-scripts'.$cff_min.'.js' ,
-	    	array('jquery'),
-	    	CFFVER,
-	    	true
-	    );
-EOD
-) );
+		$this->assertNotFalse( strpos( $content, 'echo \'var cfflinkhashtags = "\' . $cff_link_hashtags . \'";\';') );
+		$this->assertNotFalse( strpos( $content, 'wp_enqueue_script(\'cffscripts\');') );
 		$this->assertNotFalse( strpos( $content, 'add_action( \'wp_footer\', [ self::$instance, \'cff_js\' ] );') );
 	}
 }
