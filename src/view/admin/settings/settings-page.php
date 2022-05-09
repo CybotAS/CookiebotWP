82eedc0a24196b1cc0006a1f40278e486774ee36
Aytac Kokus <aytac@appsaloon.be>
2022-05-09 16:40:36 +0200

    change setting label

--- src/view/admin/settings/settings-page.php
+++ src/view/admin/settings/settings-page.php
@@ -427,7 +427,7 @@
 					</td>
 				</tr>
 				<tr>
-					<th scope="row"><?php esc_html_e( 'Cookiebot ignore enqueued scripts', 'cookiebot' ); ?></th>
+					<th scope="row"><?php esc_html_e( 'Cookiebot consent ignore enqueued scripts', 'cookiebot' ); ?></th>
 					<td>
 						<textarea
 								name="cookiebot-ignore-scripts"
