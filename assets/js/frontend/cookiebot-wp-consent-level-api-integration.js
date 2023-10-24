750eede30428a882a32bcdd81338792eaf8d0dc1
andrestamayo <jorge.tamayo@usercentrics.com>
2023-10-24 10:37:37 +0200

    Cookies with deny value not being created on first consent interaction fix

--- assets/js/frontend/cookiebot-wp-consent-level-api-integration.js
+++ assets/js/frontend/cookiebot-wp-consent-level-api-integration.js
@@ -1,30 +1,34 @@
 'use strict';
+(window => {
+    window.wp_consent_type = 'optin';
+    window.addEventListener('CookiebotOnConsentReady', cookiebot_update_consent_level, false);
+    window.addEventListener('load', cookiebot_on_load_consent_level, false);
 
-window.wp_consent_type = 'optin';
-window.addEventListener('CookiebotOnAccept', cookiebot_update_consent_level, false);
-window.addEventListener('CookiebotOnDecline', cookiebot_update_consent_level, false);
-window.addEventListener('load', cookiebot_update_consent_level, false);
-
-function cookiebot_update_consent_level() {
-  wp_set_consent('functional', 'allow'); //always allow functional cookies
-
-  const consents = new Map([['n', 1], ['p', 1], ['s', 1], ['m', 1],]);
-
-  if (typeof Cookiebot !== 'undefined') {
-    consents.set('p', Cookiebot.consent.preferences ? 1 : 0);
-    consents.set('s', Cookiebot.consent.statistics ? 1 : 0);
-    consents.set('m', Cookiebot.consent.marketing ? 1 : 0);
-  }
-
-  let consentMappingKey = Array.from(consents.entries())
-    .map(([key, value]) => `${key}=${value}`)
-    .join(';');
-
-  for (let key in cookiebot_category_mapping[consentMappingKey]) {
-    const strValue = cookiebot_category_mapping[consentMappingKey][key] ? 'allow' : 'deny';
-
-    if ((wp_has_consent(key) && strValue !== 'allow') || (!wp_has_consent(key) && strValue === 'allow')) {
-      wp_set_consent(key, strValue);
+    function cookiebot_on_load_consent_level() {
+        if (typeof Cookiebot !== 'undefined' && Cookiebot.consented === true) {
+            cookiebot_update_consent_level();
+        }
     }
-  }
-}
+
+    function cookiebot_update_consent_level() {
+        wp_set_consent('functional', 'allow'); //always allow functional cookies
+
+        const consents = new Map([['n', 1], ['p', 1], ['s', 1], ['m', 1],]);
+
+        if (typeof Cookiebot !== 'undefined') {
+            consents.set('p', Cookiebot.consent.preferences ? 1 : 0);
+            consents.set('s', Cookiebot.consent.statistics ? 1 : 0);
+            consents.set('m', Cookiebot.consent.marketing ? 1 : 0);
+        }
+
+        let consentMappingKey = Array.from(consents.entries())
+            .map(([key, value]) => `${key}=${value}`)
+            .join(';');
+
+        for (let key in cookiebot_category_mapping[consentMappingKey]) {
+            const strValue = cookiebot_category_mapping[consentMappingKey][key] ? 'allow' : 'deny';
+            wp_set_consent(key, strValue);
+        }
+    }
+
+})(window);
