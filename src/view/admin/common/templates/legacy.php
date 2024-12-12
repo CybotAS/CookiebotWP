78677a740a8c5a2057dfc6758eae8c80a619a6dd
andrestamayo <jorge.tamayo@usercentrics.com>
2024-12-12 15:13:23 +0100

    Add legacy options logic

---
+++ src/view/admin/common/templates/legacy.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @var array $options
+ */
+
+use cybot\cookiebot\settings\templates\Legacy_Settings;
+
+?>
+<div>
+<?php
+if ( ! empty( $options ) ) {
+	foreach ( $options as $option => $value ) {
+		echo Legacy_Settings::get_legacy_option( $option, $value );
+	}
+}
+?>
+</div>
