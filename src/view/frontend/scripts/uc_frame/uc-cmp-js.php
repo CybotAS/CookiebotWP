930291222cd7f16c4e0ee38292369cada271acbe
andrestamayo <jorge.tamayo@usercentrics.com>
2024-12-20 08:46:38 +0100

    Remove test code

--- src/view/frontend/scripts/uc_frame/uc-cmp-js.php
+++ src/view/frontend/scripts/uc_frame/uc-cmp-js.php
@@ -7,16 +7,15 @@
  */
 ?>
 <?php if ( $iab_enabled ) : ?>
-	<script src="https://web.cmp.usercentrics-sandbox.eu/tcf/stub.js"></script>
+	<script src="https://web.cmp.usercentrics.eu/tcf/stub.js"></script>
 <?php endif; ?>
 <?php if ( $auto ) : ?>
-	<script src="https://web.cmp.usercentrics-sandbox.eu/modules/autoblocker.js"></script>
+	<script src="https://web.cmp.usercentrics.eu/modules/autoblocker.js"></script>
 <?php endif; ?>
 <script id="usercentrics-cmp"
 		data-usercentrics="Usercentrics Consent Management Platform"
 		data-<?php echo esc_html( $ruleset_id ); ?>-id="<?php echo esc_attr( $cbid ); ?>"
-		src="https://web.cmp.usercentrics-sandbox.eu/ui/loader.js"
+		src="https://web.cmp.usercentrics.eu/ui/loader.js"
 		type="text/javascript"
-		data-sandbox="true"
 		async
 ></script>
